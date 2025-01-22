606cabcc36dd274e54ebbed008316676b52799f6
gabriel torres <gtghosth007@gmail.com>
2025-01-22 18:11:38 -0300

    Adicionando dishes e ajustando os cards dos dishes para mesma forma do burger,

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,11 @@ const products = [
   { id: 1, name: "Vegan Burger", price: 25.0, image: "./assets/img/imgProducts/burger1.png", category: "burger" },
   { id: 2, name: "Plant-Based Milk", price: 19.5, pricePromotion: 12.9, image: "./assets/img/imgProducts/burger2.png", category: "burger" },
   { id: 3, name: "Organic Tofu", price: 15.5, image: "./assets/img/imgProducts/burger3.png", category: "burger" },
-  { id: 4, name: "Vegan Cheese", price: 18.0, image: "./assets/img/imgProducts/burger4.png", category: "dish" },
+  { id: 4, name: "Vegan Cheese", price: 18.0, image: "./assets/img/imgProducts/burger4.png", category: "burger" },
+  { id: 5, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato1.png", category: "dish" },
+  { id: 6, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato2.png", category: "dish" },
+  { id: 7, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato3.png", category: "dish" },
+  { id: 8, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato4.png", category: "dish" },
 ];
 
 const burgerList = document.getElementById("burgerList");
@@ -19,31 +23,30 @@ function renderProducts(productsToRender) {
   burgers.forEach((product) => {
     const hasPromotion = product.pricePromotion !== undefined;
     const discountPercentage = hasPromotion
-        ? Math.round(((product.price - product.pricePromotion) / product.price) * 100)
-        : 0;
+      ? Math.round(((product.price - product.pricePromotion) / product.price) * 100)
+      : 0;
     const productCard = `
       <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
         <div class="card position-relative">
           ${hasPromotion ? `<div class="promo-badge"><span>${discountPercentage}%</span> <span>OFF</span></div>` : ""}
           <img src="${product.image}" class="card-img-top custom-img-product" alt="${product.name}">
           <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
+            <h5 class="card-title mt-4 mb-3">${product.name}</h5>
             <div class="prices">
               ${hasPromotion
-                ? `
-                <div>
-                  <p class="card-text custom-price-promotion text-decoration-line-through text-muted">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                  <p class="card-text custom-price">R$${product.pricePromotion.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+        ? `
+                <div class="price">
+                  <p class="card-text custom-price-promotion text-decoration-line-through text-muted mb-1">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+                  <p class="card-text custom-price mb-1">R$${product.pricePromotion.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
                 </div>
                 `
-                : `<p class="card-text custom-price mb-3">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>`
-              }
-              <div>
-                em até 3x de R$${(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} sem juros
-              </div>
-
+        : `<p class="card-text custom-price mb-1 text-center">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>`
+      }
+              <p class="fw-light text-muted text-center">
+                  <span class="fw-bold">3x</span> de <span class="fw-bold">R$${(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> sem juros
+              </p>
             </div>
-            <div class="about-products"> 
+            <div class="about-products mt-2"> 
               <button class="btn btn-buy" onclick="showDetails(${product.id})">Comprar</button>
               <button class="btn btn-eye" onclick="showProducts(${product.id})"> <i class="fa-solid fa-eye"></i> Detalhes</button>
             </div>
@@ -52,33 +55,45 @@ function renderProducts(productsToRender) {
       </div>
     `;
     burgerList.innerHTML += productCard;
-});
+  });
 
   dishes.forEach((product) => {
     const hasPromotion = product.pricePromotion !== undefined;
+    const discountPercentage = hasPromotion
+      ? Math.round(((product.price - product.pricePromotion) / product.price) * 100)
+      : 0;
     const productCard = `
-      <div class="col-md-3 mb-4">
-        <div class="card">
-          <img src="${product.image}" class="card-img-top custom-img-product" alt="${product.name}">
-          <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
-            <div class="prices">
-              ${hasPromotion
-                ? `<p class="card-text custom-price text-decoration-line-through text-muted">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                   <p class="card-text custom-price">R$${product.pricePromotion.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>`
-                : `<p class="card-text custom-price mb-3">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>`
-              }
-            </div>
-            <div class="about-products"> 
-              <button class="btn btn-buy" onclick="showDetails(${product.id})">Comprar</button>
-              <button class="btn btn-eye" onclick="showProducts(${product.id})"> <i class="fa-solid fa-eye"></i> Detalhes</button>
-            </div>
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+      <div class="card position-relative">
+        ${hasPromotion ? `<div class="promo-badge"><span>${discountPercentage}%</span> <span>OFF</span></div>` : ""}
+        <img src="${product.image}" class="card-img-top custom-img-product" alt="${product.name}">
+        <div class="card-body">
+          <h5 class="card-title mt-4 mb-3">${product.name}</h5>
+          <div class="prices">
+            ${hasPromotion
+        ? `
+              <div class="price">
+                <p class="card-text custom-price-promotion text-decoration-line-through text-muted mb-1">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+                <p class="card-text custom-price mb-1">R$${product.pricePromotion.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+              </div>
+              `
+        : `<p class="card-text custom-price mb-1 text-center">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>`
+      }
+            <p class="fw-light text-muted text-center">
+                <span class="fw-bold">3x</span> de <span class="fw-bold">R$${(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> sem juros
+            </p>
+          </div>
+          <div class="about-products mt-2"> 
+            <button class="btn btn-buy" onclick="showDetails(${product.id})">Comprar</button>
+            <button class="btn btn-eye" onclick="showProducts(${product.id})"> <i class="fa-solid fa-eye"></i> Detalhes</button>
           </div>
         </div>
       </div>
-    `;
+    </div>
+  `;
     dishList.innerHTML += productCard;
   });
+
 }
 
 function showDetails(productId) {

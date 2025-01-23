7c15a1b07616295ab659dbaf7785e679ed92418d
gabriel torres <gtghosth007@gmail.com>
2025-01-23 17:51:31 -0300

    foi criado o sidebar/menu lateral para filtragem dos produtos, também foi criado o arquivo Js para gerenciamento de abertura e fechamento do sidebar

--- assets/js/listProduct.js
+++ assets/js/listProduct.js
@@ -7,24 +7,22 @@ const products = [
   { id: 6, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato2.png", category: "dish" },
   { id: 7, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato3.png", category: "dish" },
   { id: 8, name: "Vegan Dish", price: 18.0, image: "./assets/img/imgProducts/prato4.png", category: "dish" },
+  { id: 9, name: "Vegan Burguer", price: 28.0, image: "./assets/img/imgProducts/burger5.png", category: "burger" },
+  { id: 10, name: "Vegan Pizza", price: 28.0, image: "./assets/img/imgProducts/pizza.png", category: "burger" },
 ];
 
-const burgerList = document.getElementById("burgerList");
-const dishList = document.getElementById("dishList");
+const burgerList = document.getElementById("productList");
 const searchInput = document.getElementById("searchInput");
 
 function renderProducts(productsToRender) {
-  burgerList.innerHTML = "";
-  dishList.innerHTML = "";
+  products.innerHTML = "";
 
-  const burgers = productsToRender.filter((product) => product.category === "burger");
-  const dishes = productsToRender.filter((product) => product.category === "dish");
-
-  burgers.forEach((product) => {
+  productsToRender.forEach((product) => {
     const hasPromotion = product.pricePromotion !== undefined;
     const discountPercentage = hasPromotion
       ? Math.round(((product.price - product.pricePromotion) / product.price) * 100)
       : 0;
+
     const productCard = `
       <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
         <div class="card position-relative">
@@ -54,46 +52,9 @@ function renderProducts(productsToRender) {
         </div>
       </div>
     `;
-    burgerList.innerHTML += productCard;
-  });
 
-  dishes.forEach((product) => {
-    const hasPromotion = product.pricePromotion !== undefined;
-    const discountPercentage = hasPromotion
-      ? Math.round(((product.price - product.pricePromotion) / product.price) * 100)
-      : 0;
-    const productCard = `
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-      <div class="card position-relative">
-        ${hasPromotion ? `<div class="promo-badge"><span>${discountPercentage}%</span> <span>OFF</span></div>` : ""}
-        <img src="${product.image}" class="card-img-top custom-img-product" alt="${product.name}">
-        <div class="card-body">
-          <h5 class="card-title mt-4 mb-3">${product.name}</h5>
-          <div class="prices">
-            ${hasPromotion
-        ? `
-              <div class="price">
-                <p class="card-text custom-price-promotion text-decoration-line-through text-muted mb-1">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                <p class="card-text custom-price mb-1">R$${product.pricePromotion.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-              </div>
-              `
-        : `<p class="card-text custom-price mb-1 text-center">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>`
-      }
-            <p class="fw-light text-muted text-center">
-                <span class="fw-bold">3x</span> de <span class="fw-bold">R$${(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> sem juros
-            </p>
-          </div>
-          <div class="about-products mt-2"> 
-            <button class="btn btn-buy" onclick="showDetails(${product.id})">Comprar</button>
-            <button class="btn btn-eye" onclick="showProducts(${product.id})"> <i class="fa-solid fa-eye"></i> Detalhes</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
-    dishList.innerHTML += productCard;
+    productList.innerHTML += productCard;
   });
-
 }
 
 function showDetails(productId) {
@@ -114,5 +75,5 @@ searchInput.addEventListener("input", (e) => {
   renderProducts(filteredProducts);
 });
 
-// Renderiza os produtos inicialmente
+// Renderiza todos os produtos ao carregar a página
 renderProducts(products);

f5bd89730f18da444a3699910f49123e5dd6b4fc
gabriel torres <gtghosth007@gmail.com>
2025-01-25 14:35:19 -0300

    implementando modal de informações do produto, esse modal foi implementado ao precionado o botão de 'detalhes' do produto

--- assets/js/listProduct.js
+++ assets/js/listProduct.js
@@ -1,5 +1,31 @@
 const products = [
-  { id: 1, name: "Vegan Burger", price: 25.0, image: "./assets/img/imgProducts/burger1.png", category: "burger" },
+  {
+    id: 1,
+    name: "Vegan Power Burger",
+    price: 25.0,
+    image: "./assets/img/imgProducts/burger1.png",
+    category: "burger",
+    ingredientes: [
+      { categoria: "Ingredientes principais", nome: "pão" },
+      { categoria: "Ingredientes principais", nome: "abobrinhas" },
+      { categoria: "Ingredientes principais", nome: "cebola roxa" },
+      { categoria: "Ingredientes principais", nome: "pimentão" },
+      { categoria: "Hambúrguer", nome: "200 g de feijão vermelho pré-cozido" },
+      { categoria: "Hambúrguer", nome: "2 colheres de sopa de flocos de aveia" },
+      { categoria: "Hambúrguer", nome: "1/2 cebola roxa" },
+      { categoria: "Hambúrguer", nome: "salsinha" },
+      { categoria: "Hambúrguer", nome: "1 colher de sopa de soja" },
+      { categoria: "Hambúrguer", nome: "caril" },
+      { categoria: "Hambúrguer", nome: "páprica" },
+      { categoria: "Hambúrguer", nome: "óleo EVO" },
+      { categoria: "Hambúrguer", nome: "sal" },
+      { categoria: "Maionese", nome: "120 ml de óleo de semente de girassol" },
+      { categoria: "Maionese", nome: "60 ml de leite de soja" },
+      { categoria: "Maionese", nome: "1 colher de sopa de suco de limão" },
+      { categoria: "Maionese", nome: "1 pitada de cúrcuma" },
+      { categoria: "Maionese", nome: "1 pitada de sal" }
+    ]
+  },
   { id: 2, name: "Plant-Based Milk", price: 19.5, pricePromotion: 12.9, image: "./assets/img/imgProducts/burger2.png", category: "burger" },
   { id: 3, name: "Organic Tofu", price: 15.5, image: "./assets/img/imgProducts/burger3.png", category: "burger" },
   { id: 4, name: "Vegan Cheese", price: 18.0, image: "./assets/img/imgProducts/burger4.png", category: "burger" },
@@ -72,25 +98,51 @@ const renderProducts = (page) => {
           <div class="card-body">
             <h5 class="card-title mt-4 mb-3">${product.name}</h5>
             <div class="prices">
-              ${
-                hasPromotion
-                  ? `
+              ${hasPromotion
+        ? `
                 <div class="price">
                   <p class="text-decoration-line-through mb-1 custom-price-old fs-5">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>
                   <p class="custom-price mb-1">R$${product.pricePromotion.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>
                 </div>
                 `
-                  : `<p class="custom-price mb-1 text-center">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>`
-              }
+        : `<p class="custom-price mb-1 text-center">R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>`
+      }
               <p class="fw-light text-muted text-center">
                 <span class="fw-bold">3x</span> de <span class="fw-bold">R$${(product.price / 3).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</span> sem juros
               </p>
             </div>
             <div class="about-products mt-2"> 
-              <a href="product.html?id=${product.id}"><button class="btn btn-buy text-nowrap w-auto" onclick="showDetails(${product.id})">Comprar</button></a>
-              <button class="btn btn-eye text-nowrap w-auto" onclick="showProducts(${product.id})"> <i class="fa-solid fa-eye"></i> Detalhes</button>
+              <a href="product.html?id=${product.id}"><button class="btn btn-buy text-nowrap w-auto"">Comprar</button></a>
+              <button class="btn btn-eye text-nowrap w-auto" data-toggle="modal" data-target="#exampleModalLong" onclick="showDetails(${product.id})"> <i class="fa-solid fa-eye"></i> Detalhes</button>
             </div>
           </div>
+        </div>
+
+        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">${product.name}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <ul>
+                          ${product.ingredientes.map((ing) => {
+                            ing.categoria.map((categ) => {
+                              
+                            })
+                          })}
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <a href="product.html?id=${product.id}"><button class="btn btn-buy text-nowrap w-auto"">Comprar</button></a>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     `;
@@ -101,9 +153,6 @@ const renderProducts = (page) => {
 
 const renderPagination = () => {
   const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
-
-  console.log("Total de produtos:", filteredProducts.length);
-  console.log("Total de páginas:", totalPages);
 
   paginationControls.innerHTML = "";
 
@@ -148,7 +197,6 @@ const renderPagination = () => {
     paginationControls.appendChild(pageItem);
   }
 
-  // Botão "Próximo"
   const nextButton = document.createElement("li");
   nextButton.classList.add("page-item");
   if (currentPage === totalPages) {
@@ -168,11 +216,6 @@ const renderPagination = () => {
 
 const showDetails = (productId) => {
   const product = filteredProducts.find((p) => p.id === productId);
-};
-
-const showProducts = (productId) => {
-  const product = filteredProducts.find((p) => p.id === productId);
-  alert(`Produto: ${product.name}\nPreço: R$${product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`);
 };
 
 searchInput.addEventListener("input", (e) => {
